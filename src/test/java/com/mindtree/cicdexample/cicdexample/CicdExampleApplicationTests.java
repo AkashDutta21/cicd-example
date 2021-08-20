a4844d2968d0fef328129a3f596bb9ea61c59aa5
Akash Dutta <Akash.Dutta2@mindtree.com>
2021-08-20 10:07:05 +0530

    Initial Commit

---
+++ src/test/java/com/mindtree/cicdexample/cicdexample/CicdExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindtree.cicdexample.cicdexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
